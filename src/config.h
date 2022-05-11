4d7fb4fe41c25297920729be249d73f14973c44e
Louis Adamian <louis@adamian.net>
2022-05-11 16:47:15 -0400

    add base for swarm transmision

--- src/config.h
+++ src/config.h
@@ -6,4 +6,7 @@
 #define WHALE_TAG_RECOVERY_CONIFIG_H
 #define r2rDac8
 #include "TagRecoveryBoardv0_9.h"
+#define APRS_SSID 2
+#define TAG_SN 2
+#define APRS_CALL "KC1QXQ"
 #endif //WHALE_TAG_RECOVERY_CONIFIG_H
--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,27 @@
-#if (ARDUINO >= 100)
 #include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
 #include <SparkFun_Swarm_Satellite_Arduino_Library.h>
 #include "APRS.hh"
 #include "Swarm.hh"
 #include "TagRecoveryBoardv0_9.h"
+#include "LittleFS.h"
 
-enum recovery_board_state_t : uint8_t {in_setup=0, pre_application=1, sleep=2, transmitting=3, recovery=4};
+enum recovery_board_state_t : uint8_t {in_setup=0, sleeping=2, transmitting=3, recovery=4};
+uint8_t dive_count; // dive count is updated by message from main board
+recovery_board_state_t state;
 SWARM_M138 swarm;
+APRS aprs(APRS_CALL, APRS_SSID);
+byte* swarmTxPacket;
+uint8_t battery_level;
+uint16_t detach;
 
-APRS aprs("KC1QXQ", 1);
-char comment[] = " v0.9 https://projectceti.org";
+LittleFSConfig cfg;
 
-struct swarmTxPacket{
-
-};
 
 void setup() {
     SerialPIO vhfSerial = SerialPIO(dra818vUartRxPin, dra818vUartTxPin);
     APRS::configDra818v(vhfSerial);
-
+    cfg.setAutoFormat(false);
+    LittleFS.setConfig(cfg);
 }
 
 void loop() {
@@ -30,7 +30,26 @@ void loop() {
 
 
 void txAprs(){
+  auto *gpsQuality = new Swarm_M138_GPS_Fix_Quality_t;
+  swarm.getGpsFixQuality(gpsQuality);
+  if(gpsQuality->fix_type != SWARM_M138_GPS_FIX_TYPE_NF && gpsQuality->fix_type != SWARM_M138_GPS_FIX_TYPE_TT && gpsQuality->fix_type !=SWARM_M138_GPS_FIX_TYPE_INVALID){
+    auto *gps = new Swarm_M138_GeospatialData_t;
+    swarm.getGeospatialInfo(gps);
+    uint8_t dive_count; // get dive count from main board
+    // get battery state from
+    char message[6];
+    message[0] = TAG_SN;
+    message[1] = (char)state;
+    message[3] = battery_level;
+    message[4] = detach & 10;
+    message[5] = detach & 01;
+    aprs.sendPacket(gps, message);
+    delete gps;
+  }
+  else{
 
+  }
+  delete gpsQuality;
 }
 
 uint8_t initTag(){
@@ -60,8 +79,23 @@ uint8_t recoveryLoop(){
     return 0;
 }
 
-uint8_t txSwarm(){
-    return 0;
+uint8_t queueSwarmTx(){
+  uint32_t currTime;
+  byte currMessage[9];
+  currMessage[0] = (uint8_t) state;
+  currMessage[1] = dive_count;
+  currMessage[2] = battery_level;
+  currMessage[3] = detach & 10;
+  currMessage[4] = detach & 01;
+  currMessage[5] = currTime & 0xFF000000;
+  currMessage[6] = currTime & 0x00FF0000;
+  currMessage[7] = currTime & 0x0000FF00;
+  currMessage[8] = currTime & 0x000000FF;
+  uint64_t msgId;
+  Swarm_M138_Error_e err = swarm.transmitBinaryExpire(currMessage,9,&msgId, currTime+600);
 }
 
 
+uint8_t writeGpsLog(){
+
+}
