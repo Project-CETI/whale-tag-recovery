34d9de8102d1cdaa10e8b75d65269ec7192d4515
Shashank Swaminathan <sh.swami235@gmail.com>
2022-07-10 21:29:46 -0400

    Storing uf2s for all tags: regular op and no APRS op

--- src/main.c
+++ src/main.c
@@ -13,11 +13,11 @@ uint16_t aCS[3] = {0,0,0};
 
 // APRS communication config (change per tag)
 char mycall[8] = "KC1QXQ";
-int myssid = 2;
+int myssid = 10;
 char dest[8] = "APLIGA";
 char digi[8] = "WIDE2";
 int digissid = 1;
-char comment[128] = "Ceti b1.0 Tag2";
+char comment[128] = "Ceti b1.0 2-#";
 uint32_t aprsInterval = 120000; // APRS TX interval
 
 // SWARM communication config (change ONLY when necessary)
@@ -41,7 +41,7 @@ void initLed(void);
 void setup(void);
 
 void set_bin_desc() {
-  bi_decl(bi_program_description("Recovery process binary for Tag 2."));
+  bi_decl(bi_program_description("Recovery process binary for standalone recovery board 2-#"));
   bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
   bi_decl(bi_1pin_with_name(TX_SWARM, "TX UART to SWARM modem"));
   bi_decl(bi_1pin_with_name(RX_SWARM, "RX UART to SWARM modem"));
