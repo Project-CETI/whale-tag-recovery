86f5ff406cbe461502958af0f7d702301e4d92ae
Shashank Swaminathan <sh.swami235@gmail.com>
2022-08-08 17:35:19 -0400

    First round of var revamp

--- recovery/gps.c
+++ recovery/gps.c
@@ -6,58 +6,32 @@
 #include "gps.h"
 #include "minmea.h"
 
-// GPS DEFS [START] ---------------------------------------------------
-#define MAX_GPS_MSG_LEN 500
-// GPS DEFS [END] -----------------------------------------------------
-
-// GPS VARS [START] ---------------------------------------------------
-// GPS communication processes
-uart_inst_t *uart;
-char gps_rd_buf[MAX_GPS_MSG_LEN];
-int gps_rd_buf_pos = 0;
-int gps_buf_len = 0;
-char endNmeaString = '*';
-uint8_t nmeaCS;
-bool sInteractive = false;
-bool datCheck, posiCheck;
-
-// GPS data hacks
-float latlonBuf[2] = {42.3648,0};
-uint16_t acsBuf[3] = {0,0,0};
-char lastGpsBuffer[MAX_GPS_MSG_LEN] = "$GN 42.3648,-71.1247,0,360,0*38";
-int lastGpsBufSize = 31;
-char lastDtBuffer[MAX_GPS_MSG_LEN] = "$DT 20190408195123,V*41";
-int lastDtBufSize = 23;
-
-// GPS VARS [END] -----------------------------------------------------
-
 // GPS FUNCTIONS [START] ----------------------------------------------
 // RX functions from NEO-M8N
 
-void parseGpsOutput(char *line) {
+void parseGpsOutput(char *line, int buf_len, gps_data_s * gps_dat) {
   switch (minmea_sentence_id(line, false)) {
   case MINMEA_SENTENCE_RMC: {
     struct minmea_sentence_rmc frame;
     if (minmea_parse_rmc(&frame, line)) {
-      memcpy(lastGpsBuffer, gps_rd_buf, gps_buf_len);
-      latlonBuf[0] = minmea_tocoord(&frame.latitude);
-      latlonBuf[1] = minmea_tocoord(&frame.longitude);
-      if (isnan(latlonBuf[0])) latlonBuf[0] = 42.3648;
-      if (isnan(latlonBuf[1])) latlonBuf[1] = 0.0;
-      acsBuf[1] = minmea_rescale(&frame.course, 3);
-      acsBuf[2] = minmea_rescale(&frame.speed, 1);
-      lastGpsBufSize = gps_buf_len;
-      // printf("[PARSED]: %f, %f, %d, %d\n", latlonBuf[0], latlonBuf[1], acsBuf[1], acsBuf[2]);
-      printf("[C/S]: %d, %d, %d, %d, %d, %d\n", &frame.course.value, &frame.course.scale, acsBuf[0], &frame.speed.value, &frame.speed.scale, acsBuf[1]);
-			posiCheck = true;
+      memcpy(gps_dat->lastGpsBuffer, line, buf_len);
+      gps_dat->latlon[0] = minmea_tocoord(&frame.latitude);
+      gps_dat->latlon[1] = minmea_tocoord(&frame.longitude);
+      if (isnan(gps_dat->latlon[0])) gps_dat->latlon[0] = 42.3648;
+      if (isnan(gps_dat->latlon[1])) gps_dat->latlon[1] = 0.0;
+      gps_dat->acs[1] = minmea_rescale(&frame.course, 3);
+      gps_dat->acs[2] = minmea_rescale(&frame.speed, 1);
+      // printf("[PARSED]: %f, %f, %d, %d\n", gps_dat->latlon[0], gps_dat->latlon[1], gps_dat->acs[1], gps_dat->acs[2]);
+      // printf("[C/S]: %d, %d, %d, %d, %d, %d\n", &frame.course.value, &frame.course.scale, gps_dat->acs[0], &frame.speed.value, &frame.speed.scale, gps_dat->acs[1]);
+			gps_dat->posCheck = true;
     }
     break;
   }
   case MINMEA_SENTENCE_ZDA: {
     struct minmea_sentence_zda frame;
-    if (minmea_parse_zda(&frame, line)) memcpy(lastDtBuffer, gps_rd_buf, gps_buf_len);
-    lastDtBufSize = gps_buf_len;
-    // printf("[DT]: %s", lastDtBuffer);
+    if (minmea_parse_zda(&frame, line))
+			memcpy(gps_dat->lastDtBuffer, line, buf_len);
+    // printf("[DT]: %s", gps_dat->lastDtBuffer);
     break;
   }
   case MINMEA_INVALID:
@@ -67,65 +41,52 @@ void parseGpsOutput(char *line) {
   }
 }
 
-void readFromGps(void) {
+void readFromGps(const gps_config_s * gps_cfg, gps_data_s * gps_dat) {
   char inChar;
-  if (uart_is_readable(uart)) {
-    inChar = uart_getc(uart);
+	char gps_rd_buf[MAX_GPS_MSG_LEN];
+	int gps_rd_buf_pos = 0;
+	int gps_buf_len = 0;
+  if (uart_is_readable(gps_cfg->uart)) {
+    inChar = uart_getc(gps_cfg->uart);
     if (inChar == '$') {
-			datCheck = true;
+			gps_dat->datCheck = true;
       int i = 0;
       gps_rd_buf[i++] = inChar;
       while (inChar != '\r') {
-        inChar = uart_getc(uart);
+        inChar = uart_getc(gps_cfg->uart);
 				gps_rd_buf[i++] = inChar;
       }
       gps_rd_buf[i-1] = '\0';
       gps_buf_len = i-1;
-      parseGpsOutput(gps_rd_buf);
+      parseGpsOutput(gps_rd_buf, gps_buf_len, gps_dat);
       // printf("%s\n",gps_rd_buf);
     }
   }
 }
 
-void drainGpsFifo(void) {
-	if (uart_is_readable(uart)) uart_read_blocking(uart, gps_rd_buf, uart_is_readable(uart));
-	datCheck = false;
-	posiCheck = false;
+void drainGpsFifo(const gps_config_s * gps_cfg, gps_data_s * gps_dat) {
+	char gps_rd_buf[MAX_GPS_MSG_LEN];
+	if (uart_is_readable(gps_cfg->uart))
+		uart_read_blocking(gps_cfg->uart,
+											 gps_rd_buf,
+											 uart_is_readable(gps_cfg->uart));
+	gps_dat->datCheck = false;
+	gps_dat->posCheck = false;
 }
 
-void echoGpsOutput(void) {
-  for (int i = 0; i < gps_buf_len; i++) {
-    if (gps_rd_buf[i]=='\r') puts_raw("\n[NEW]");
-    if (gps_rd_buf[i]=='\n') printf("[%d]",i);
-    putchar_raw(gps_rd_buf[i]);
+void echoGpsOutput(char *line, int buf_len) {
+  for (int i = 0; i < buf_len; i++) {
+    if (line[i]=='\r') puts_raw("\n[NEW]");
+    if (line[i]=='\n') printf("[%d]",i);
+    putchar_raw(line[i]);
   }
 }
 
 // Init NEO-M8N functions
-void gpsInit(int txPin, int rxPin, int baudrate, uart_inst_t *uartNum, bool *dCheck, bool *pCheck) {
-  // store vars
-  uart = uartNum;
-	datCheck = dCheck;
-	posiCheck = pCheck;
-  // init uart
-  uart_init(uart, baudrate);
-  gpio_set_function(txPin, GPIO_FUNC_UART);
-  gpio_set_function(rxPin, GPIO_FUNC_UART);
-}
-
-// Data retrieval functions
-
-void getPos(float *dstPosBuf) {
-  memcpy(dstPosBuf, latlonBuf, sizeof(latlonBuf));
-}
-
-void getACS(uint16_t *dstACSBuf) {
-  memcpy(dstACSBuf, acsBuf, sizeof(acsBuf));
-}
-
-void getLastPDtBufs(char *dstGpsBuf, char *dstDtBuf) {
-  memcpy(dstGpsBuf, lastGpsBuffer, lastGpsBufSize);
-  memcpy(dstDtBuf, lastDtBuffer, lastDtBufSize);
+void gpsInit(const gps_config_s * gps_cfg) {
+	uart_init(gps_cfg->uart, gps_cfg->baudrate);
+	gpio_set_function(gps_cfg->txPin, GPIO_FUNC_UART);
+	gpio_set_function(gps_cfg->rxPin, GPIO_FUNC_UART);
 }
 
 // GPS FUNCTIONS [END] ------------------------------------------------
--- recovery/tag.c
+++ recovery/tag.c
@@ -5,51 +5,43 @@
 #define MAX_TAG_MSG_LEN 20
 // TAG DEFS [END] -----------------------------------------------------
 
-// TAG VARS [START] -----------------------------------------------------
-// do any of these operations?
-uart_inst_t *uartTag;
-// reading/writing from/to tag
-char tag_rd_buf[MAX_TAG_MSG_LEN];
-uint32_t tMsgSent = 0;
-uint32_t ackWaitT_us= 1000;
-uint32_t ackWaitT_ms= 1000;
-int numTries = 3;
-bool writeDataToTag = false;
-// TAG VARS [END] -------------------------------------------------------
-
 // TAG FUNCTIONS [START] ------------------------------------------------
 int parseTagStatus(void) {
   return 0;
 }
 
-void writeGpsToTag(char *lastGps, char *lastDt) {
-  uart_putc(uartTag, 'g');
-  uart_puts(uartTag, lastGps);
-  uart_puts(uartTag, lastDt);
-  uart_putc(uartTag, '\n');
+void writeGpsToTag(const tag_config_s * tag_cfg, char *lastGps, char *lastDt) {
+  uart_putc(tag_cfg->uart, 'g');
+  uart_puts(tag_cfg->uart, lastGps);
+  uart_puts(tag_cfg->uart, lastDt);
+  uart_putc(tag_cfg->uart, '\n');
 }
 
-void detachTag(void) {
+void detachTag(const tag_config_s * tag_cfg) {
+	char tag_rd_buf[MAX_TAG_MSG_LEN];
+	uint32_t tMsgSent = 0;
   tMsgSent = to_ms_since_boot(get_absolute_time());
-  while(to_ms_since_boot(get_absolute_time()) - tMsgSent < ackWaitT_ms) {
-    if (!uart_is_writable(uartTag)) uart_tx_wait_blocking(uartTag);
-    uart_putc(uartTag, 'D');
-    uart_putc(uartTag, '\n');
-    if (uart_is_readable_within_us(uartTag, ackWaitT_us)) {
-      uart_read_blocking(uartTag, tag_rd_buf, 1);
+  while(to_ms_since_boot(get_absolute_time()) - tMsgSent < tag_cfg->ackWaitT_ms) {
+    if (!uart_is_writable(tag_cfg->uart)) uart_tx_wait_blocking(tag_cfg->uart);
+    uart_putc(tag_cfg->uart, 'D');
+    uart_putc(tag_cfg->uart, '\n');
+    if (uart_is_readable_within_us(tag_cfg->uart, tag_cfg->ackWaitT_us)) {
+      uart_read_blocking(tag_cfg->uart, tag_rd_buf, 1);
       if (tag_rd_buf[0] == 'd') break;
     }
-    busy_wait_ms(ackWaitT_ms/numTries);
+    busy_wait_ms(tag_cfg->ackWaitT_ms/tag_cfg->numTries);
   }
 }
 
-void reqTagState(void) {
+void reqTagState(const tag_config_s * tag_cfg) {
+	char tag_rd_buf[MAX_TAG_MSG_LEN];
+	uint32_t tMsgSent = 0;
   tMsgSent = to_ms_since_boot(get_absolute_time());
-  uart_putc(uartTag, 'T');
-  uart_putc(uartTag, '\n');
-  while (to_ms_since_boot(get_absolute_time()) - tMsgSent < ackWaitT_ms) {
-    if (uart_is_readable_within_us(uartTag, ackWaitT_us)) {
-      uart_read_blocking(uartTag, tag_rd_buf, 5);
+  uart_putc(tag_cfg->uart, 'T');
+  uart_putc(tag_cfg->uart, '\n');
+  while (to_ms_since_boot(get_absolute_time()) - tMsgSent < tag_cfg->ackWaitT_ms) {
+    if (uart_is_readable_within_us(tag_cfg->uart, tag_cfg->ackWaitT_us)) {
+      uart_read_blocking(tag_cfg->uart, tag_rd_buf, 5);
       parseTagStatus();
       break;
     }
@@ -57,10 +49,9 @@ void reqTagState(void) {
 }
 
 // Init functions
-void initTagComm(int txPin, int rxPin, int baudrate, uart_inst_t *uartNum) {
-  uartTag = uartNum;
-  uart_init(uartTag, baudrate);
-  gpio_set_function(txPin, GPIO_FUNC_UART);
-  gpio_set_function(rxPin, GPIO_FUNC_UART);
+void initTagComm(const tag_config_s * tag_cfg) {
+  uart_init(tag_cfg->uart, tag_cfg->baudrate);
+  gpio_set_function(tag_cfg->txPin, GPIO_FUNC_UART);
+  gpio_set_function(tag_cfg->rxPin, GPIO_FUNC_UART);
 }
 // TAG FUNCTIONS [END] --------------------------------------------------
