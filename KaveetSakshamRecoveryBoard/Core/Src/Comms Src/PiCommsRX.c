8c0c5039b65378a14a00c9954f253f5082d251b5
kgrewal26 <kaveetgrewal@gmail.com>
2023-08-21 11:33:54 -0400

    tuned state transitions

--- KaveetSakshamRecoveryBoard/Core/Src/Comms Src/PiCommsRX.c
+++ KaveetSakshamRecoveryBoard/Core/Src/Comms Src/PiCommsRX.c
@@ -11,6 +11,10 @@
 #include "main.h"
 #include "stm32u5xx_hal_uart.h"
 
+//static function helpers.
+static bool stopRecovery(bool * isActive);
+static bool stopGPSCollection(bool * isActive);
+
 //External variables
 extern UART_HandleTypeDef huart2;
 extern Thread_HandleTypeDef threads[NUM_THREADS];
@@ -64,19 +68,51 @@ void pi_comms_rx_thread_entry(ULONG thread_input){
 
 void pi_comms_parse_message(RX_Message_IDs message_id, uint8_t * payload_pointer, uint8_t payload_length){
 
+	static bool isRecovery = false;
+	static bool isGPSCollect = false;
+
 	switch (message_id) {
 
 		case START_RECOVERY:
+
+			//Stop GPS data collection if its running (function will handle checking if its active or not)
+			stopGPSCollection(&isGPSCollect);
+
 			//Start recovery
 			tx_thread_resume(&threads[APRS_THREAD].thread);
+			isRecovery = true;
+
+			/*if GPS collection is active, suspend it (so only one thread that accesses the GPS hardware is active at a time)
+			if (isGPSCollect) {
+				tx_thread_suspend(&threads[GPS_COLLECTION_THREAD].thread);
+				isGPSCollect = false;
+			}*/
+
 			break;
 
 		case STOP:
-			//Stop recovery
-			tx_thread_suspend(&threads[APRS_THREAD].thread);
+
+			//Stop recovery or GPS collection (function will handle checking if its active or not)
+			stopRecovery(&isRecovery);
+			stopGPSCollection(&isGPSCollect);
+
 			break;
 
 		case START_GPS_COLLECTION:
+
+			//Call to suspend recovery if it is active (function will handle checking if its active or not)
+			stopRecovery(&isRecovery);
+
+			//Start Normal GPS collection
+			tx_thread_resume(&threads[GPS_COLLECTION_THREAD].thread);
+			isGPSCollect = true;
+
+			//if APRS recovery is active, suspend it (so only one thread that accesses the GPS hardware is active at a time)
+			/*if (isRecovery) {
+				tx_thread_suspend(&threads[APRS_THREAD].thread);
+				isRecovery = false;
+			}*/
+
 			break;
 		default:
 			//Bad message ID - do nothing
@@ -85,3 +121,27 @@ void pi_comms_parse_message(RX_Message_IDs message_id, uint8_t * payload_pointer
 	}
 
 }
+
+//Checks to see if recovery is active, if it is, we stop it.
+static bool stopRecovery(bool * isActive){
+
+	//if APRS recovery is active, suspend it (so only one thread that accesses the GPS hardware is active at a time)
+	if (*isActive){
+		tx_thread_suspend(&threads[APRS_THREAD].thread);
+		*isActive = false;
+	}
+
+}
+
+//Checks to see if GPS data collection is active, if it is, we stop it.
+static bool stopGPSCollection(bool * isActive){
+
+	//if GPS data collecion is active, suspend it (so only one thread that accesses the GPS hardware is active at a time)
+	if (*isActive){
+		tx_thread_suspend(&threads[GPS_COLLECTION_THREAD].thread);
+		*isActive = false;
+	}
+
+}
+
+
